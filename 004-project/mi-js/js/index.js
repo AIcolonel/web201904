4ee6e7d0e06c851553f61a193928d4a217a8956b
captainchen <1101145957@qq.com>
2019-06-02 17:34:47 +0800

    mi-js-nav

--- 004-project/mi-js/js/index.js
+++ 004-project/mi-js/js/index.js
@@ -26,3 +26,87 @@ function handleCart(){
 		});
 	}
 }
+
+//2.处理导航栏
+handleNav();
+function handleNav(){
+	//1.获取元素
+	var aNavItem = document.querySelectorAll(".header .header-nav-item");
+	var oNavContent = document.querySelector(".header .header-nav-content");
+	var oContainer = oNavContent.querySelector('.container');
+	var hideTimer = null;
+	//2.循环遍历每一个li,绑定事件
+	for(var i =0;i<aNavItem.length-2;i++){
+		aNavItem[i].index = i;
+		aNavItem[i].onmouseenter = function(){
+			clearTimeout(hideTimer);
+			//js显示上边框
+			oNavContent.style.borderTop = "1px solid #ccc";
+			//动画显示导航下拉列表
+			animate(oNavContent,{height:200},false,function(){
+				oNavContent.style.overflow = "visible";
+			});
+
+			//动态加载数据
+			loadData(this.index);
+		}
+		aNavItem[i].onmouseleave = function(){
+			/*
+			hideTimer = setTimeout(function(){
+				oNavContent.style.overflow = "hidden";
+				//动画隐藏导航下拉列表
+				animate(oNavContent,{height:0},false,function(){
+					//js隐藏上边框
+					oNavContent.style.borderTop = "none";
+				});
+			},300)
+			*/
+			hideNav();
+		}
+	}
+	//鼠标移入导航下拉列表显示内容
+	oNavContent.onmouseenter = function(){
+		clearTimeout(hideTimer);
+	}
+	//鼠标移出导航下拉列表隐藏内容
+	oNavContent.onmouseleave = function(){
+		hideNav();
+	}
+
+	//加载数据函数
+	function loadData(index){
+		var data = aNavItemData[index];
+		// console.log(data);
+		var html = '';
+			html += '<ul>'
+			for(var i = 0;i<data.length;i++){
+				html +=		'<li>';
+				html +=		'	<a href="'+data[i].url+'">';
+				html +=		'		<div class="img-box">';
+				html +=		'			<img src="'+data[i].src+'" alt="">';
+				html +=		'		</div>';
+				html +=		'		<p class="header-nav-name">'+data[i].name+'</p>';
+				html +=		'		<p class="header-nav-price">'+data[i].price+'元</p>';
+				if(data[i].tag){
+					html +=		'		<span class="tag">'+data[i].tag+'</span>';
+				}
+				html +=		'	</a>';
+				html +=		'</li>';
+			}
+			html += '</ul>'
+
+		oContainer.innerHTML = html;
+	}
+
+	//隐藏列表共通函数
+	function hideNav(){
+		hideTimer = setTimeout(function(){
+			oNavContent.style.overflow = "hidden";
+			//动画隐藏导航下拉列表
+			animate(oNavContent,{height:0},false,function(){
+				//js隐藏上边框
+				oNavContent.style.borderTop = "none";
+			});
+		},300)
+	}
+}
