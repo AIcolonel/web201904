a8f896c0aabaed827eb165199c98a078cee70138
captainchen <1101145957@qq.com>
2019-06-15 17:20:43 +0800

    ajax

---
+++ 005-js-middle/045-server2-readfile.js
@@ -0,0 +1,27 @@
+var http = require('http');
+
+var fs = require('fs');
+
+var server = http.createServer(function(req,res){
+	console.log(req.url);
+	if(req.url == "/favicon.ico"){
+		res.end("favicon.ico");
+	}
+
+	//http://127.0.0.1/045-test.html
+	var filePath  = "./" + req.url;
+	fs.readFile(filePath,function(err,data){
+		if(err){
+			res.statusCode = 404;
+			res.end("not found!!!");
+		}else{
+			res.end(data);
+		}
+	})
+
+	// res.end("hello");
+});
+
+server.listen(3000,"10.196.12.235",function(){
+	console.log("server is running at http://10.196.12.235:3000");
+});
