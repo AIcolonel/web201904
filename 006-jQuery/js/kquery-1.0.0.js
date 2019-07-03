73a57c50157cf7fb8a3671ad2175f9967fc4ed0f
captainchen <1101145957@qq.com>
2019-07-03 17:33:38 +0800

    jquery selector done

--- 006-jQuery/js/kquery-1.0.0.js
+++ 006-jQuery/js/kquery-1.0.0.js
@@ -17,12 +17,58 @@
 				document.addEventListener('DOMContentLoaded',selector);
 				return this;
 			}
+			//3.字符串
+			else if(kQuery.isString(selector)){
+				//3.1代码片段
+				if(kQuery.isHTML(selector)){
+					var tempDiv = document.createElement('div');
+					tempDiv.innerHTML = selector;
+					for(var i = 0;i<tempDiv.children.length;i++){
+						this[i] = tempDiv.children[i]
+					}
+					this.length = tempDiv.children.length;
+
+					return this;
+
+				}else{//3.2选择器
+					var doms = document.querySelectorAll(selector);
+					for(var i = 0;i<doms.length;i++){
+						this[i] = doms[i];
+					}
+					this.length = doms.length;
+					return this;
+				}
+			}
+			//4.数组
+			else if(kQuery.isArray(selector)){
+				for(var i = 0;i<selector.length;i++){
+					this[i] = selector[i];
+				}
+				this.length = selector.length;
+				return this;
+			}
+			//5.对象(其他)
+			else{
+				this[0] = selector;
+				this.context = document;
+				this.length = 1;
+				return this;
+			}
 		}
 	}
 
 	/*kQuery的静态方法*/
 	kQuery.isFunction = function(str){
 		return typeof str == 'function';
+	}
+	kQuery.isString = function(str){
+		return typeof str == 'string';
+	}
+	kQuery.isHTML = function(str){
+		return /<[^<>]+>/.test(str);
+	}
+	kQuery.isArray = function(str){
+		return typeof str == 'object' && (length in str);
 	}
 
 
