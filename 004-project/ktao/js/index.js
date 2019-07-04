26473fdca808e5f68af9b95377514838b7957de4
captainchen <1101145957@qq.com>
2019-07-04 17:59:19 +0800

    showhide

--- 004-project/ktao/js/index.js
+++ 004-project/ktao/js/index.js
@@ -1,1 +1,12 @@
-console.log($)
+(function($){
+	var $dropdown = $('.dropdown');
+	$dropdown
+	.hover(function(){
+		// console.log($(this).data('active'));
+		var activeClass = $(this).data('active') + "-active"
+		$(this).addClass(activeClass);
+	},function(){
+		var activeClass = $(this).data('active') + "-active"
+		$(this).removeClass(activeClass);
+	})
+})(jQuery);
