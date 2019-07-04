b9e3d0cfb7e6bd103e010621b8730e819d939294
captainchen <1101145957@qq.com>
2019-07-04 10:24:41 +0800

    ktao ready

--- 006-jQuery/js/kquery-1.0.0.js
+++ 006-jQuery/js/kquery-1.0.0.js
@@ -54,10 +54,38 @@
 				this.length = 1;
 				return this;
 			}
+		},
+		get:function(index){
+			//判断是否有参数
+			if(index || index == 0){
+				//是否是数字
+				if(kQuery.isNumber(index)){
+					if(index >= 0){
+						return this[index]
+					}else{//-1 4  -2 3 -3 2
+						return this[this.length + index]
+					}
+				}
+			}else{
+				var arr = [];
+				for(var i = 0;i<this.length;i++){
+					arr[i] = this[i];
+				}
+
+				return arr;
+			}
 		}
 	}
 
+	kQuery.extend = kQuery.prototype.extend = function(options){
+		for(var key in options){
+			this[key] = options[key];
+		}
+	}
+
+
 	/*kQuery的静态方法*/
+	/*
 	kQuery.isFunction = function(str){
 		return typeof str == 'function';
 	}
@@ -70,6 +98,27 @@
 	kQuery.isArray = function(str){
 		return typeof str == 'object' && (length in str);
 	}
+	kQuery.isNumber = function(str){
+		return typeof str == 'number';
+	}
+	*/
+	kQuery.extend({
+		isFunction : function(str){
+			return typeof str == 'function';
+		},
+		isString : function(str){
+			return typeof str == 'string';
+		},
+		isHTML : function(str){
+			return /<[^<>]+>/.test(str);
+		},
+		isArray : function(str){
+			return typeof str == 'object' && (length in str);
+		},
+		isNumber : function(str){
+			return typeof str == 'number';
+		}
+	})
 
 
 	kQuery.fn.init.prototype = kQuery.fn;
