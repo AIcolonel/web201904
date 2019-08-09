0621263dfb69fc97f8c2099c913fecfda48cce7c
captainchen <1101145957@qq.com>
2019-08-09 16:16:48 +0800

    react-state-props

---
+++ 014-react/03-todolist/src/App.js
@@ -0,0 +1,11 @@
+// export const a = 1;
+/*
+const a = 1;
+const b = 2;
+export {
+	a as a1,
+	b
+}
+*/
+const a = 1
+export default a;
---
+++ 014-react/03-todolist/src/index.js
@@ -0,0 +1,6 @@
+// import {a1,b as b1} from './App.js';
+
+// console.log(a1,b1);
+
+import a from './App.js';
+console.log(a);
